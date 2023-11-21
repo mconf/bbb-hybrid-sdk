744a65b40fac7d162fb006b6b09816e50d0fcbf0
Gabriel <gabriel@example.com>
2023-11-21 11:10:31 -0300

    created 4 buttons, stylized containers

--- src/screens/home-screen/index.js
+++ src/screens/home-screen/index.js
@@ -1,17 +1,36 @@
-import { View } from 'react-native';
+import s from './styles'
 import { useNavigation } from '@react-navigation/native';
 import Card from '../../components/card';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   return (
-    <View>
-      <Card
-        title="Enquetes"
-        icon="poll"
-        onPress={() => navigation.navigate('PollScreen')}
-      />
-    </View>
+    <s.Container>
+      <s.LineContainer>
+        <Card
+          title="Enquetes"
+          icon="poll"
+          onPress={() => navigation.navigate('PollScreen')}
+        />
+        <Card
+          title="Perguntas"
+          icon="poll"
+          onPress={() => navigation.navigate('PollScreen')}
+        />
+      </s.LineContainer>
+      <s.LineContainer>
+        <Card
+          title="Chat"
+          icon="poll"
+          onPress={() => navigation.navigate('PollScreen')}
+        />
+        <Card
+          title="Apresentacao"
+          icon="poll"
+          onPress={() => navigation.navigate('PollScreen')}
+        />
+      </s.LineContainer>
+    </s.Container>
   );
 };
 
